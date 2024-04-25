d4338e30d47bd1ac2a7fe5cfd2d93c572b7d388f
Osvaldo Ojeda <osvaldo.devmate@gmail.com>
2024-04-25 20:05:04 -0300

    fs

--- fsSincronico/index.js
+++ fsSincronico/index.js
@@ -1,7 +1,16 @@
 // const fs = require("node:fs");
 // console.log(`🚀 ~ fs:`, fs.readFileSync)
 
-const { readFileSync, writeFileSync } = require("node:fs");
+const {
+  readFileSync,
+  writeFileSync,
+  readdirSync,
+  appendFileSync,
+  mkdirSync,
+  unlinkSync,
+  statSync,
+} = require("node:fs");
+// console.log(`🚀 ~ readdirSync:`, readdirSync())
 
 const read = () => {
   try {
@@ -22,6 +31,52 @@ const write = (data) => {
   }
 };
 
-read();
+const readDir = () => {
+  try {
+    const dir = readdirSync("./").forEach((e) => console.log(e));
+    // console.log(`🚀 ~ readDir ~ dir:`, dir)
+  } catch (chayane) {
+    console.log(`🚀 ~ readDir ~ error:`, chayane);
+  }
+};
 
-write("QUE ONDAAAAA");
+const append = () => {
+  try {
+    const appendFile = appendFileSync("./readme.md", `\njueves`);
+    console.log(`🚀 ~ append ~ appendFile:`, appendFile);
+  } catch (error) {
+    console.log(`🚀 ~ append ~ error:`, error);
+  }
+};
+const mkDir = () => {
+  try {
+    const mk = mkdirSync("./prueba");
+    console.log(`🚀 ~ append ~ appendFile:`, mk);
+  } catch (error) {
+    console.log(`🚀 ~ append ~ error:`, error);
+  }
+};
+const unlink = () => {
+  try {
+    const unlink = unlinkSync("./readme.md");
+    console.log(`🚀 ~ append ~ appendFile:`, unlink);
+  } catch (error) {
+    console.log(`🚀 ~ append ~ error:`, error);
+  }
+};
+const stat = () => {
+  try {
+    const stat = statSync("./fsSincronico");
+    console.log(`🚀 :`, stat);
+  } catch (error) {
+    console.log(`🚀 ~ append ~ error:`, error);
+  }
+};
+
+// read();
+// write("QUE ONDAAAAA");
+// readDir()
+// append()
+// mkDir()
+// unlink()
+// stat()
